d41f891dbaa078312026312b984942725874ecdf
Than Nguyen <rivon.programmer@gmail.com>
2017-04-20 13:20:22 +0700

    Create project

---
+++ SimpleBankSystem/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBankSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleBankSystem/Models/Entity/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBankSystem.Models.Entity
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
